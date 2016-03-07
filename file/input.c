1c1eaa8c4de69a5ff207b2c4ff10d561023862fc
aemonair <v.aemonair@gmail.com>
2016-03-07 19:57:46 +0800

    test_file_io

---
+++ file/input.c
@@ -0,0 +1,44 @@
+//love heyue
+
+Air//_
+
+//The movie //which u look
+
+//where can i go*/
+
+// *//* what's that?
+
+// /**/all right.
+
+/* Miss
+ *//*
+ */
+
+/* moviing
+//*/^_^
+
+
+/*a
+//b
+//c
+//d
+//e
+//f
+*/
+
+/* Hi~ *//*where are u
+           */
+
+/*
+ *//*
+      */hey
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Love u
+
+"Hey, i want to see u.*/"
+
+"天气变好啦!!~~~"
+
+/*Hey hey ,* what's the one.***/
+
+//Come on,"hu~*/tarid"
